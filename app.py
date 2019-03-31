1642f895b5fb9d589ecd3a199c030c448dd30368
Christo MS <csicking95@gmail.com>
2019-03-31 16:47:55 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-
 from flask import Flask
-from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'a2ul0j0$'
+app.config['MYSQL_DB'] = 'StatePopulation'
+mysql = MySQL(app)
 
-@app.route("/")
-def main():
-    return render_template('index.html')
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM Population_new''')
+    rv = cur.fetchall()
+    return str(rv)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug = True)
